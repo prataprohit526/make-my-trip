2c3de95e2bf443fa8ad1310e86636a76e3baa0fc
prataprohit526 <prataprohit526@gmail.com>
2023-08-28 10:41:54 +0530

    add all

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,44 @@
 import React from "react";
-import './Footer.css';
-import {FaFacebookF, FaTwitter} from 'react-icons/fa';
+import "./Footer.css";
+import { FaFacebookF, FaTwitter } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-const Footer= ()=>{
-    return(
-        <div className="footer">
-            <div className="footer-contents">
-                <div className="media-link">
-                    <h1><a href="nj" target={'_blank'}><FaTwitter /></a></h1>
-                    <h1><a href="ede" target={'_blank'}><FaFacebookF /></a></h1>
-                </div>
-                <div className="copyright">
-                    <h3>© 2023 <Link to={'/'}>MAKEMYTRIP CLONE</Link></h3> by Shantanu
-                    <br/>
-                    Country <h3><Link to={'/india'}>India</Link> USA UAE</h3>
-                </div>
-            </div>
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-contents">
+        <div className="media-link">
+          <h1>
+            <a href="nj" target={"_blank"}>
+              <FaTwitter />
+            </a>
+          </h1>
+          <h1>
+            <a href="ede" target={"_blank"}>
+              <FaFacebookF />
+            </a>
+          </h1>
         </div>
-    )
-}
+        <div className="copyright">
+          <h3>
+            © 2023 <Link to={"/"}>MAKEMYTRIP CLONE</Link>
+          </h3>{" "}
+          by Rohit
+          <br />
+          Country{" "}
+          <h3>
+            <a
+              href={
+                "/https://www.linkedin.com/in/rohit-pratap-yadav-9b3780258/"
+              }
+            >
+              India
+            </a>
+            USA UAE
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Footer;
